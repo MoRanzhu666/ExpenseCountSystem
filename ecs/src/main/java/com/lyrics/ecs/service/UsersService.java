f2a5fb642cc046dc58124a06f96c8692427b69bf
luoying <3358778839@qq.com>
2025-10-01 12:23:32 +0800

    1.添加token校验

--- ecs/src/main/java/com/lyrics/ecs/service/UsersService.java
+++ ecs/src/main/java/com/lyrics/ecs/service/UsersService.java
@@ -1,5 +1,6 @@
 package com.lyrics.ecs.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.lyrics.ecs.bean.po.UsersPo;
 import com.lyrics.ecs.mapper.UsersMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,5 +13,11 @@ public class UsersService {
 
     public UsersPo getById(String id) {
         return usersMapper.selectById(id);
+    }
+
+    public UsersPo getByNamePWD(String username, String password) {
+        return usersMapper.selectOne(new QueryWrapper<UsersPo>().lambda()
+                .eq(UsersPo::getName, username)
+                .eq(UsersPo::getPassword, password));
     }
 }
